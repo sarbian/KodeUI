31fed90bae2a962ab6c577d20723f5d52007db73
Bill Currie <bill@taniwha.org>
2020-09-07 19:25:21 +0900

    Add an empty ui element
    It's purpose is to facilitate flexible space between layout elements.

---
+++ KodeUI-Unity/Assets/KodeUI/UIEmpty.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace KodeUI
+{
+    class UIEmpty : Layout
+    {
+        public override void CreateUI()
+        {
+        }
+
+        public override void Style()
+        {
+        }
+    }
+}
