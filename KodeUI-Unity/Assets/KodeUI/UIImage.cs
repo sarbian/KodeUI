29413fd7125f1055ac69128d41b6e1c621cc23bb
Bill Currie <bill@taniwha.org>
2020-09-21 20:01:30 +0900

    Add support for tweaking UIImage

--- KodeUI-Unity/Assets/KodeUI/UIImage.cs
+++ KodeUI-Unity/Assets/KodeUI/UIImage.cs
@@ -6,12 +6,13 @@ namespace KodeUI
 {
     class UIImage : UIObject
     {
-		public Image image { get; private set; }
+        public Image image { get; private set; }
 
         public override void CreateUI()
         {
             Pivot(PivotPresets.TopLeft);
-			image = gameObject.AddComponent<Image>();
+            image = gameObject.AddComponent<Image>();
+            image.color = UnityEngine.Color.white;
         }
 
         public override void Style()
@@ -20,7 +21,19 @@ namespace KodeUI
 
         public UIImage Image(Sprite sprite)
         {
-			image.sprite = sprite;
+            image.sprite = sprite;
+            return this;
+        }
+
+        public UIImage Type(Image.Type type)
+        {
+            image.type = type;
+            return this;
+        }
+
+        public UIImage Color(Color color)
+        {
+            image.color = color;
             return this;
         }
     }
