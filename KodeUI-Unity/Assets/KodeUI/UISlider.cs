a053d70eebf0a0dc5591fbf6df5b1078d1ae991d
Bill Currie <bill@taniwha.org>
2020-09-21 20:06:24 +0900

    Implement sliders
    Very low level (need to add background, fill and handle as separate
    objects).

---
+++ KodeUI-Unity/Assets/KodeUI/UISlider.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace KodeUI
+{
+	class UISlider : UIObject
+	{
+		public Slider slider { get; private set; }
+
+		UIObject fill;
+		public UIObject Fill
+		{
+			get { return fill; }
+			set {
+				fill = value;
+				if (fill != null) {
+					slider.fillRect = fill.rectTransform;
+				} else {
+					slider.fillRect = null;
+				}
+			}
+		}
+
+		UIObject handle;
+		public UIObject Handle
+		{
+			get { return handle; }
+			set {
+				handle = value;
+				if (handle != null) {
+					slider.handleRect = handle.rectTransform;
+				} else {
+					slider.handleRect = null;
+				}
+			}
+		}
+
+		public override void CreateUI()
+		{
+			slider = gameObject.AddComponent<Slider>();
+		}
+
+		public override void Style()
+		{
+		}
+
+		public UISlider Direction(Slider.Direction direction)
+		{
+			slider.direction = direction;
+			return this;
+		}
+	}
+}
