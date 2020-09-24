92e1503adb0a71c00fa1203d9fda8b97ab2e446a
Bill Currie <bill@taniwha.org>
2020-09-24 19:19:48 +0900

    Fix up slider to work with a visible knob

--- KodeUI-Unity/Assets/KodeUI/UISlider.cs
+++ KodeUI-Unity/Assets/KodeUI/UISlider.cs
@@ -9,16 +9,25 @@ namespace KodeUI
 		public Slider slider { get; private set; }
 
 		UIImage background;
-		UIImage handle;
 		UIImage fill;
+		UIEmpty fillArea;
+		UIImage handle;
+		UIEmpty handleArea;
 
 		public override void CreateUI()
 		{
 			slider = gameObject.AddComponent<Slider>();
 
-			Add<UIImage> (out background, "Background").Type(Image.Type.Sliced).Anchor(AnchorPresets.StretchAll).SizeDelta(0, 0);
-			Add<UIImage> (out handle, "Handle");
-			Add<UIImage> (out fill, "Fill").Type(Image.Type.Sliced).Anchor(AnchorPresets.StretchAll).SizeDelta(0, 0);
+			var anchorMin = new Vector2 (0, 0.25f);
+			var anchorMax = new Vector2 (1, 0.75f);
+
+			Add<UIImage> (out background, "Background").Type(Image.Type.Sliced).Anchor(anchorMin, anchorMax).SizeDelta(0, 0).Finish();
+			Add<UIEmpty>(out fillArea, "Fill Area").Anchor(anchorMin, anchorMax).SizeDelta (-20, 0).X(-5).Y(0).Pivot(PivotPresets.MiddleCenter)
+				.Add<UIImage> (out fill, "Fill").Type(Image.Type.Sliced).SizeDelta(10, 0).Pivot(PivotPresets.MiddleCenter).Finish()
+			.Finish();
+			Add<UIEmpty>(out handleArea, "Handle Area").Anchor(AnchorPresets.StretchAll).SizeDelta (-20, 0)
+				.Add<UIImage> (out handle, "Handle").SizeDelta(20, 0).Pivot(PivotPresets.MiddleCenter).Finish()
+			.Finish();
 
 			slider.handleRect = handle.rectTransform;
 			slider.fillRect = fill.rectTransform;
@@ -45,6 +54,14 @@ namespace KodeUI
 		public UISlider Handle(bool enabled)
 		{
 			handle.gameObject.SetActive (enabled);
+			if (enabled) {
+				float width = handle.rectTransform.sizeDelta.x;
+				fillArea.X(-width / 4).WidthDelta(-width);
+				fill.WidthDelta(width/2);
+			} else {
+				fillArea.X(0).WidthDelta(0);
+				fill.WidthDelta(0);
+			}
 			return this;
 		}
 	}
