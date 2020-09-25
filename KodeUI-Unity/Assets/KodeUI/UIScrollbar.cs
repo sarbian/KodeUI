61aeab611c66fcedccb939a3e716c640f9ea01cb
Bill Currie <bill@taniwha.org>
2020-09-25 22:10:50 +0900

    Clean up UIScrollbar to use KodeUI elements for children
    Yet another step closer to better style handling.

--- KodeUI-Unity/Assets/KodeUI/UIScrollbar.cs
+++ KodeUI-Unity/Assets/KodeUI/UIScrollbar.cs
@@ -7,40 +7,30 @@ namespace KodeUI
 	public class UIScrollbar : UIObject
 	{
 		Image image;
-		RectTransform slidingArea;
+		UIEmpty slidingArea;
+		UIImage handle;
 		public Scrollbar scrollbar { get; private set; }
-		Image handle;
 
 		public override void CreateUI()
 		{
 			image = gameObject.AddComponent<Image>();
 			image.color = UnityEngine.Color.white;
-			ImageLoader.SetupImage (image, "KodeUI/Default/background");
+			image.type = Image.Type.Sliced;
 
 			scrollbar = gameObject.AddComponent<Scrollbar>();
 
-			var go = new GameObject("Sliding Area", typeof(RectTransform));
-			slidingArea = go.transform as RectTransform;
-			slidingArea.SetParent (rectTransform, true);
-			slidingArea.SetAnchor(AnchorPresets.StretchAll);
-			slidingArea.sizeDelta = Vector2.zero;
+			Add<UIEmpty> (out slidingArea, "Sliding Area").Anchor(AnchorPresets.StretchAll).SizeDelta(0, 0)
+				.Add<UIImage> (out handle, "Handle").Type(Image.Type.Sliced).Anchor(AnchorPresets.StretchAll).SizeDelta(0, 0).Finish()
+			.Finish();
 
-			go = new GameObject("Handle Rect", typeof (RectTransform));
-			var handleRect = go.transform as RectTransform;
-			handleRect.SetParent (slidingArea, true);
-			handleRect.SetAnchor(AnchorPresets.StretchAll);
-			handleRect.sizeDelta = Vector2.zero;
-			scrollbar.handleRect = handleRect;
-			handle = go.AddComponent<Image>();
-			handle.color = UnityEngine.Color.white;
-			ImageLoader.SetupImage (handle, "KodeUI/Default/background");
+			scrollbar.handleRect = handle.rectTransform;
 		}
 
 		public override void Style()
 		{
 			scrollbar.colors = style.stateColors ?? ColorBlock.defaultColorBlock;
-			handle.sprite = style.standard;
-			handle.color = style.color ?? UnityEngine.Color.white;
+			handle.image.sprite = style.standard;
+			handle.image.color = style.color ?? UnityEngine.Color.white;
 
 			image.sprite = style.background;
 			image.color = style.color ?? UnityEngine.Color.white;
