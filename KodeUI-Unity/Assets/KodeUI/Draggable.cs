62d019be4ee54d20900d26ac9c906812215ebafe
Bill Currie <bill@taniwha.org>
2020-11-26 18:51:36 +0900

    Add a component to ease dragging custom windows
    There's something slightly wrong with its offset, but it's good enough
    for now.

---
+++ KodeUI-Unity/Assets/KodeUI/Draggable.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+namespace KodeUI
+{
+    public class Draggable : UIBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler
+    {
+        Vector2 dragAnchor;
+
+		RectTransform rectTransform
+		{
+			get { return transform as RectTransform; }
+		}
+
+        void DoDrag(PointerEventData eventData)
+        {
+            Vector2 currentPos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, eventData.position, eventData.pressEventCamera, out currentPos);
+            Vector3 localDelta = currentPos - dragAnchor;
+            rectTransform.localPosition += localDelta;
+        }
+
+        public virtual void OnDrag(PointerEventData eventData)
+        {
+            DoDrag(eventData);
+        }
+
+        public virtual void OnBeginDrag(PointerEventData eventData)
+        {
+            DoDrag(eventData);
+        }
+
+        public virtual void OnEndDrag(PointerEventData eventData)
+        {
+            DoDrag(eventData);
+        }
+
+        public virtual void OnPointerDown(PointerEventData eventData)
+        {
+			if (eventData.button == PointerEventData.InputButton.Left) {
+				RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, eventData.position, eventData.pressEventCamera, out dragAnchor);
+			}
+        }
+    }
+}
